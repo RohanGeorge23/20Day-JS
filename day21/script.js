14e1ea49c01707c79cdfa4f09d6f17bbdc671d69
RohanGeorge23 <rohangeorge77@gmail.com>
2022-02-18 13:26:08 +0530

    day 21

---
+++ day21/script.js
@@ -0,0 +1,37 @@
+setTimeout(() => {
+    console.log(10)
+    document.body.innerHTML=`<h1>10</h1>`
+    
+    setTimeout(() => {
+        console.log(9)
+        document.body.innerHTML=`<h1>9</h1>`
+        setTimeout(() => {
+            document.body.innerHTML=`<h1>8</h1>`
+            setTimeout(() => {
+                document.body.innerHTML=`<h1>7</h1>`
+                setTimeout(() => {
+                    document.body.innerHTML=`<h1>6</h1>`
+                    setTimeout(() => {
+                        document.body.innerHTML=`<h1>5</h1>`
+                        setTimeout(() => {
+                            document.body.innerHTML=`<h1>4</h1>`
+                            setTimeout(() => {
+                                document.body.innerHTML=`<h1>3</h1>`
+                                setTimeout(() => {
+                                    document.body.innerHTML=`<h1>2</h1>`
+                                    setTimeout(() => {
+                                        document.body.innerHTML=`<h1>1</h1>`
+                                        setTimeout(() => {
+                                            document.body.innerHTML=`<h1>Happy Independence Day!!🧡</h1>`
+                                           
+                                        }, 1000);
+                                    }, 1000);
+                                }, 1000);
+                            }, 1000);
+                        }, 1000);
+                    }, 1000);
+                }, 1000);
+            }, 1000);
+        }, 1000);
+    }, 1000);
+}, 1000);
